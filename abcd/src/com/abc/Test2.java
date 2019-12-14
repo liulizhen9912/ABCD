e3a3cee4a83d8bac52a48d2e6e7358f8bf52d1cb
000 <000@000-PC>
2019-12-15 01:16:10 +0800

    fenzhi

--- abcd/src/com/abc/Test2.java
+++ abcd/src/com/abc/Test2.java
@@ -1,9 +1,8 @@
 package com.abc;
 
-public class Test {
-	public static void main(String[] args) {
-		int a=1;
-		
-	}
+public class Test2 {
 
+	public static void main(String[] args) {
+		System.out.println("nihao sad fasdf asd");
+	}
 }
